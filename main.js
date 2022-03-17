5e6dd3cbb19b0b9b7eef98b7bad7a8ac8eb8fbd6
Fadd Space <fadd@fadd.space>
2022-03-17 18:41:53 +0300

    1 commit

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ module.exports.loop = function(){
     for(const creppIndex in Game.creeps){
         const creep =Game.creeps[creppIndex];
 
-        const source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE)
+        const source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+        
     }
 }
