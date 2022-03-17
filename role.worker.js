8dfc03e4db22b46e7dee7ce04b488d9549be3ee5
Fadd Space <fadd@fadd.space>
2022-03-17 20:51:07 +0300

    2

--- role.worker.js
+++ role.worker.js
@@ -2,8 +2,10 @@ const isWorking = require('ISCREEPWORKING');
 
 
 
-function LocateSorces(creep){
+function LocateSources(creep){
     const source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+
+
     if(creep.memory.isWorking == true && creep.harvest(source) == ERR_NOT_IN_RANGE){ creep.moveTo(source); }
 
 
@@ -12,9 +14,11 @@ function LocateSorces(creep){
 
 module.exports = {
     run: function(creep) {
-        const source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
-        isWorking(creep);
-        if(creep.memory.isWorking == false && creep.transfer(Game.spawns["CORE"], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){creep.moveTo["CORE"]}
+        isWorking.run(creep);
+        const energy _.filter(FIND_MY_STRUCTURES, (s) => (s.structureType == STRUCTURE_SPAWN || s.structureType == STRUCTURE_EXTENTION) && s.energy < s.energyCapacity);
+
+
+        if(creep.memory.isWorking == false && creep.transfer(energy, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){creep.moveTo(energy)}
         else{ LocateSorces(creep); }
      
 
